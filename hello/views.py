2d0ad81b7f83bff8290db623b7aa50bfc7fe2ec7
Jarosław Partyka <partyka.jaroslaw@gmail.com>
2023-10-23 10:38:56 +0200

    add index.html

--- hello/views.py
+++ hello/views.py
@@ -3,8 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
-
+    return render(request, "hello/index.html")
 def jarek(request):
     return HttpResponse("Hello, Jarek!")
 
